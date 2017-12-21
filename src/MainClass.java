d432216e94a59c50401e3cd31d208cde7970b18e
gvg2000 <gvg2000@yandex.ru>
2017-12-22 00:15:06 +0300

    Binary Tree

--- src/MainClass.java
+++ src/MainClass.java
@@ -10,9 +10,28 @@ import java.io.InputStreamReader;
 
 public class MainClass {
     public static void main(String[] args){
-        Task1();
+
+        //Реализовать простейшую хеш-функцию. На вход функции подаётся строка, на выходе сумма кодов символов.
+//        Task1();
+
+        Task2();
     }
 
+    private static void Task2() {
+
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(3, 1);
+        tree.insert(2, 2);
+        tree.insert(4, 5);
+        tree.print();
+        tree.remove(2);
+        tree.remove(3);
+        tree.print();
+        tree.remove(4);
+
+    }
+
+    //Реализовать простейшую хеш-функцию. На вход функции подаётся строка, на выходе сумма кодов символов.
     private static void Task1() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter string:");
