4447246739c97ee2c089b20698c68e3cf9ce17a3
gvg-github <gvg2000@yandex.ru>
2017-12-25 15:29:50 +0400

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,31 +2,41 @@
 * Гурьевских В.Г.
 *
 * (Task1) Реализовать простейшую хеш-функцию. На вход функции подаётся строка, на выходе сумма кодов символов.
+* (Task2) Переписать программу, реализующее двоичное дерево поиска.
+            а) Добавить в него обход дерева различными способами;
+            б) Реализовать поиск в двоичном дереве поиска;
+            в) *Добавить в программу обработку командной строки с помощью которой можно указывать
+из какого файла считывать данные, каким образом обходить дерево
 * */
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args){
 
         //Реализовать простейшую хеш-функцию. На вход функции подаётся строка, на выходе сумма кодов символов.
-//        Task1();
-
+        Task1();
+        //Переписать программу, реализующее двоичное дерево поиска.
         Task2();
     }
 
+    //Двоичное дерево поиска
     private static void Task2() {
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Select bypass metod: 1 for pre-order, 2 for in-order, 3 for post-order");
+        int mode = in.nextInt();
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(3, 1);
         tree.insert(2, 2);
         tree.insert(4, 5);
-        tree.print();
+        tree.print(mode);
         tree.remove(2);
         tree.remove(3);
-        tree.print();
+        tree.print(mode);
         tree.remove(4);
 
     }
